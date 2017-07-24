e864eaeaa236b5ad473061c01d47a4fda293d05e
playpool513 <playpool513@gmail.com>
2017-07-24 19:08:20 +1000

    Add name column to zombie table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170722152153) do
+ActiveRecord::Schema.define(version: 20170724090702) do
 
   create_table "zombies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
