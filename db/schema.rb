d1d01a5f8e0b0b77c16a7903ffb24acbad05bbbd
playpool513 <playpool513@gmail.com>
2017-07-25 16:54:19 +1000

    $ rails g model weapon zombie_id:integer && rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725061026) do
+ActiveRecord::Schema.define(version: 20170725065344) do
 
   create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "zombie_id"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "weapons", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "zombie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
